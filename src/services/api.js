b5a9cb2054814c4160ac8843a91e135bf48b755e
Muharram <hafizhfakhri25@gmail.com>
2026-06-11 14:47:08 +0700

    correct baseURL format in axios instance

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://kasirpintar-api.vercel.app',
+  baseURL: 'https://kasirpintar-api.vercel.app/',
 });
 
 api.interceptors.request.use(
